87a80a253f43011461203995e3a8927a51e26ff9
Ernest Tsang <kwaimuk@Ernests-MacBook-Air.local>
2017-04-08 11:19:34 -1000

    Step 19 on Beers completed

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -241,7 +241,7 @@ var livingOrganismClassification=["Kingdom", "Phylum", "Class", "Order", "Family
  * "cvs"
  *
  */
-
+var scmList = ["git", "svn", "mercurial", "bazaar", "cvs"];
 
 /* Step 19
  *
@@ -260,7 +260,14 @@ var livingOrganismClassification=["Kingdom", "Phylum", "Class", "Order", "Family
  *                  "Golden"
  *
  */
-
+var beers = {
+              IPA         : "Ale",
+              Lager       : "Strong",
+              Heffeweisen : "German",
+              Stout       : ["Thick", "Dark"],
+              Porter      : "Bitter",
+              Ale         : ["Light", "Golden"]
+            };
 
 /* Step 20
  *

2441e7469ed17febe088958ce38615fff98b1f2e
Ernest Tsang <kwaimuk@Ernests-MacBook-Air.local>
2017-04-08 10:51:05 -1000

    Step 15 DNA is now declared

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -193,6 +193,7 @@ var linuxFlavors = ["Gentoo", "Fedora", "Debian", "Slackware", "Red Hat","Bieber
  *
  */
 
+var DNA = ["nucleatides", "guanine", "adenine", "thymine", "cytosine"];
 
 /* Step 16
  *
